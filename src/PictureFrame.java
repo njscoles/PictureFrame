5b8a32911771755d64fbb6108895b34a0bece9f3
njscoles <nicholas@scolesfamily.net>
2021-12-03 17:55:04 -0600

    Added File button to top of JFrame with sub-buttons Save & Exit, Exit is fully functional. Also added the Help button to the top of JFrame with sub-button About that displays my name and the date the project was created.

--- src/PictureFrame.java
+++ src/PictureFrame.java
@@ -1,22 +1,70 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class PictureFrame extends JFrame {
-	public static void main(String[] args) {
-		String file = "descriptions.txt";
-		PictureDataReader.readPictureDataFromFile(file);
-		PictureFrame mmf = new PictureFrame();
-		mmf.setVisible(true);
 	
+	public void setupPictureFrame() {
 		JMenuBar mbar = new JMenuBar();
 		JMenu mnuFile = new JMenu("File");
 		JMenu mnuHelp = new JMenu("Help");
 		mbar.add(mnuFile);
 		mbar.add(mnuHelp);
-		//setTitle("Picture Frame");
-		//setSize(290, 400);
+		JMenuItem miSave = new JMenuItem("Save");
+		JMenuItem miExit = new JMenuItem("Exit");
+		
+		// Save button frame
+		mnuFile.add(miSave);
+		miSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Hello!");
+			}
+		});
+		// Exit button frame
+		mnuFile.add(miExit);
+		miExit.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+					}
+				});
+		
+		// About button
+		JMenuItem miAbout = new JMenuItem("About");
+		miAbout.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(null, "Created by Nick Scoles in December, 2021");
+					}
+				});
+		mnuFile.add(miSave);
+		mnuFile.add(miExit);
+		mnuHelp.add(miAbout);
+		setJMenuBar(mbar);
+	}
+	
+	public void setupGUI() {
+		setTitle("Picture Frame");
+		setSize(290, 400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setupPictureFrame();
+	}
+	
+	public PictureFrame() {
+		setupPictureFrame();
+	}
+	public static void main(String[] args) {
+		String file = "descriptions.txt";
+		PictureDataReader.readPictureDataFromFile(file);
+		PictureFrame mmf = new PictureFrame();
+		mmf.setVisible(true);
+
         
-       // setDefaultCloseOperation(EXIT_ON_CLOSE);
+      
 	}
 }
