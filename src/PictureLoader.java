f382c0f16cd0f16e99026cc7df44bc07394123a1
njscoles <nicholas@scolesfamily.net>
2021-12-03 16:52:59 -0600

    Began work on the PictureLoader class by creating a function an an arraylist

--- src/PictureLoader.java
+++ src/PictureLoader.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 
 public class PictureLoader {
-
+	private static String filename, date, description;
+	public static ArrayList<BufferedImage> loadImagesFromPictureData (String file) {
+		ArrayList<BufferedImage> bufferedImage = new ArrayList<BufferedImage>();
+	}
 }
