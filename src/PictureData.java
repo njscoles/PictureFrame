1cc3c784e6ddc30473964f8d9d4634887ed2f4ff
njscoles <nicholas@scolesfamily.net>
2021-12-02 19:51:05 -0600

    Added the variables, getters/setters, a constructor, and a toString function (commented out) to the PictureData file

--- src/PictureData.java
+++ src/PictureData.java
@@ -1,4 +1,35 @@
 
 public class PictureData {
-
+// Stores the filename, data, and description from each image
+	// Variables
+	private String filename;
+	private String date;
+	private String description;
+	
+	// Getters and Setters
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	// Constructorr
+	public PictureData(String filename, String date, String description) {
+		setFilename(filename);
+		setDate(date);
+		setDescription(description);
+	}
 }
